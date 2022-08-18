a01bdb230e46d6f7644b3975343f0c0f601fde1b
Daniel Bremer <5bremer@informatik.uni-hamburg.de>
2022-08-18 21:26:49 +0200

    huge commit, bundle database stuff and inherit it, rewwrite data.json to server new format

---
+++ src/kacky_eventpage_backend/db_ops/db_operator.py
@@ -0,0 +1,8 @@
+from kacky_eventpage_backend.db_ops.db_base import DBConnection
+
+
+class MiscDBOperators(DBConnection):
+    def get_map_author(self, kackyid: int):
+        query = "SELECT author FROM maps WHERE kacky_id = ?"
+        self._cursor.execute(query, (str(kackyid),))
+        return self._cursor.fetchone()[0]
